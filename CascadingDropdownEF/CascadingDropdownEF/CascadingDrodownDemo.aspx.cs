8bba01ffc1e451769c959689cc838188ca9d35b6
jalpesh vadgama <jalpesh.vadgama@gmail.com>
2016-04-23 09:33:11 +0530

    Added cascading dropdown web page.

---
+++ CascadingDropdownEF/CascadingDropdownEF/CascadingDrodownDemo.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CascadingDropdownEF.Models;
+
+namespace CascadingDropdownEF
+{
+    public partial class CascadingDrodownDemo : System.Web.UI.Page
+    {
+        private readonly StudentContext _studentContext;
+
+        public CascadingDrodownDemo()
+        {
+            _studentContext = new StudentContext();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
