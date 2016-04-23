d38dceacf93669cda504197f85212e8b18add6fd
jalpesh vadgama <jalpesh.vadgama@gmail.com>
2016-04-23 09:27:48 +0530

    Added Student Context.

---
+++ CascadingDropdownEF/CascadingDropdownEF/Models/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace CascadingDropdownEF.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("DefaultConnectionString")
+        {
+            
+        }
+        public DbSet<Standard> Standards { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
